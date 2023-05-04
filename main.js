54dfd631e3aa9e58952a661aae56d801b0d3fd98
Bianca Aparecida <biancaaparecida07@gmail.com>
2023-05-04 17:12:05 -0300

    funcao-anonima-e-declarada-metodo-addEventListener

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const roboton = document.querySelector("#roboton");
 
-roboton.addEventListener("click",dizoi);
+/*Uso de função anônima. Função anônima: uso apenas uma vez, executada quando o gatilho for disparado, que neste caso é o clique no botão.
+Pode ser vista de duas formas: 
+roboton.addEventListener("click",function(){console.log("Cliquei no robô!");});
+OU
+roboton.addEventListener("click",() => {console.log("Cliquei no robô!");});
+*/
 
-function dizoi(){
-    console.log("oi");
+roboton.addEventListener("click",(evento) => {console.log(evento);});
+
+//Funções conhecidas/declaradas são e podem ser usadas em várias pares do código. 
+function dizoi(nome){
+    console.log("Oi, "+nome);
+    console.log("Bem vindo ao Robotron 2000");
 }
+
+dizoi("Bianca");
