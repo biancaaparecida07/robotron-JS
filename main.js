784b5233485c77b4e2606c4e515fea5cbec73beb
Bianca Aparecida <biancaaparecida07@gmail.com>
2023-05-08 10:38:43 -0300

    arrayDeControle-funcaoAnonima-target-textContent

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
-const roboton = document.querySelector("#roboton");
+//const roboton = document.querySelector("#roboton");
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
+
+const controle = document.querySelectorAll(".controle-ajuste");
 
 /*Uso de função anônima. Função anônima: uso apenas uma vez, executada quando o gatilho for disparado, que neste caso é o clique no botão.
 Pode ser vista de duas formas: 
 roboton.addEventListener("click",function(){console.log("Cliquei no robô!");});
 OU
 roboton.addEventListener("click",() => {console.log("Cliquei no robô!");});
-*/
-
-roboton.addEventListener("click",(evento) => {console.log(evento);});
 
 //Funções conhecidas/declaradas são e podem ser usadas em várias pares do código. 
-function dizoi(nome){
-    console.log("Oi, "+nome);
-    console.log("Bem vindo ao Robotron 2000");
-}
+*/
+controle.forEach((elemento) =>{
+    elemento.addEventListener("click",(evento) => {
+        manipulaDados(evento.target.textContent); //target: retorna onde o evento ocorreu. Qual foi o botão. 
+    });
+});
 
-dizoi("Bianca");
+function manipulaDados(operacao){
+    if (operacao==="-"){
+        braco.value = (parseInt(braco.value))-1;
+    } else if (operacao==="+"){
+        braco.value = (parseInt(braco.value))+1;
+    }
+}
