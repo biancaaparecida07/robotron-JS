2907dc59c1ef4af2969ad872d07f6252336f5a35
Bianca Aparecida <biancaaparecida07@gmail.com>
2023-05-08 17:36:27 -0300

    parentNodes-dataAttribute

--- main.js
+++ main.js
@@ -1,28 +1,24 @@
 //const roboton = document.querySelector("#roboton");
-const subtrair = document.querySelector("#subtrair");
-const somar = document.querySelector("#somar");
-const braco = document.querySelector("#braco");
-
-const controle = document.querySelectorAll(".controle-ajuste");
+const controle = document.querySelectorAll("[data-controle]");
 
 /*Uso de função anônima. Função anônima: uso apenas uma vez, executada quando o gatilho for disparado, que neste caso é o clique no botão.
 Pode ser vista de duas formas: 
 roboton.addEventListener("click",function(){console.log("Cliquei no robô!");});
 OU
-roboton.addEventListener("click",() => {console.log("Cliquei no robô!");});
-
+roboton.addEventListener("click",() => {console.log("Cliquei no robô!");});*/
 //Funções conhecidas/declaradas são e podem ser usadas em várias pares do código. 
-*/
+
 controle.forEach((elemento) =>{
     elemento.addEventListener("click",(evento) => {
-        manipulaDados(evento.target.textContent); //target: retorna onde o evento ocorreu. Qual foi o botão. 
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode); //target: retorna onde o evento ocorreu. Qual foi o botão. ParentNode: é o elemento pai de onde foi clicado.
     });
 });
 
-function manipulaDados(operacao){
+function manipulaDados(operacao, controle){
+    const peca = controle.querySelector("[data-contador]");
     if (operacao==="-"){
-        braco.value = (parseInt(braco.value))-1;
+        peca.value = (parseInt(peca.value))-1;
     } else if (operacao==="+"){
-        braco.value = (parseInt(braco.value))+1;
+        peca.value = (parseInt(peca.value))+1;
     }
 }
