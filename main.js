8f5fc97a7c1b84ab860b23012a2b4aec3a27c252
Bianca Aparecida <biancaaparecida07@gmail.com>
2023-05-10 16:48:30 -0300

    atualizaEstatisticas-forEach

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
 //const roboton = document.querySelector("#roboton");
 const controle = document.querySelectorAll("[data-controle]");
+const estatisticas = document.querySelectorAll("[data-estatistica]");
+console.log(estatisticas);
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
 
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 /*Uso de função anônima. Função anônima: uso apenas uma vez, executada quando o gatilho for disparado, que neste caso é o clique no botão.
 Pode ser vista de duas formas: 
 roboton.addEventListener("click",function(){console.log("Cliquei no robô!");});
@@ -11,6 +45,7 @@ roboton.addEventListener("click",() => {console.log("Cliquei no robô!");});*/
 controle.forEach((elemento) =>{
     elemento.addEventListener("click",(evento) => {
         manipulaDados(evento.target.dataset.controle, evento.target.parentNode); //target: retorna onde o evento ocorreu. Qual foi o botão. ParentNode: é o elemento pai de onde foi clicado.
+        atualizaestatisticas(evento.target.dataset.peca);
     });
 });
 
@@ -22,3 +57,13 @@ function manipulaDados(operacao, controle){
         peca.value = (parseInt(peca.value))+1;
     }
 }
+
+function atualizaestatisticas(peca){
+    console.log(pecas[peca]);
+
+    estatisticas.forEach((elemento) => {
+        console.log(elemento.dataset.estatistica);
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica];
+    })}
+
+
